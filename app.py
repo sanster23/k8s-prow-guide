725661cc5db477a1ee8200e2dee79557b34a3134
sanster23 <shekhawatsanjay23@gmail.com>
2019-04-08 14:30:01 +0530

    update app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     myhost = os.uname()[1]
-    return 'Hello world from the other side!'
+    return 'Hello world from containers!!!!!!!!'
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
